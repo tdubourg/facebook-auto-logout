73d1fe7ef8ed6f91be8873492d7fd88e9002bea3
Tim Taubert <tim@timtaubert.de>
2012-01-02 13:45:28 +0100

    fixing top level window detection

--- data/pagemod.js
+++ data/pagemod.js
@@ -1,7 +1,7 @@
 addEventListener("DOMContentLoaded", function onLoad() {
   removeEventListener("DOMContentLoaded", onLoad, false);
 
-  if (location != parent.location)
+  if (location == parent.location)
     self.port.emit("load");
 });
 
