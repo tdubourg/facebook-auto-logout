f0905546a449afbfdf7706ac80c36552483186ca
Tim Taubert <tim@timtaubert.de>
2012-01-02 13:27:29 +0100

    detect if facebook is loaded in an iframe and don't count that as an active page

--- data/pagemod.js
+++ data/pagemod.js
@@ -1,3 +1,10 @@
+addEventListener("DOMContentLoaded", function onLoad() {
+  removeEventListener("DOMContentLoaded", onLoad, false);
+
+  if (location != parent.location)
+    self.port.emit("load");
+});
+
 addEventListener("unload", function onUnload() {
   removeEventListener("unload", onUnload, false);
   self.port.emit("unload");
